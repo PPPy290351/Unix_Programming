e5aed3eb78755f857f58d416e6866347ffe1d012
PPPy290351 <testforssid03@gmail.com>
2021-01-10 23:30:22 +0800

    hello driver

--- driver/hello.c
+++ driver/hello.c
@@ -1,1 +1,19 @@
-#
+#include <linux/init.h>
+#include <linux/module.h>
+
+
+// @my_init is the driver initializtion entry point 
+// @my_init() is called during system startup. <- If driver is statically compiled into the kernel
+// @or when the module is inserted into the kernel, depend on the driver is type of module or kernel function.
+static int my_init(void)
+{
+    return 0;
+}
+
+static void my_exit(void)
+{
+    return;
+}
+
+module_init( my_init );
+module_exit( my_exit );
